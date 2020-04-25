de8d954418940366818bb3da1cc656feded807d0
ElicsonLi <elicsonli@qq.com>
2020-04-25 16:39:57 -0400

    add login url in settings.py

--- proj_gitlight/settings.py
+++ proj_gitlight/settings.py
@@ -70,6 +70,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'proj_gitlight.wsgi.application'
 
+LOGIN_URL = '/gitlight/login'
+
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
