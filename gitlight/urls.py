11a2864ec155e563048a148e0ea0d1725c2f4e4e
SuboOx <zjuzihaocao@gmail.com>
2020-04-25 21:36:19 -0400

    Modified profile page and added list all issues function.

--- gitlight/urls.py
+++ gitlight/urls.py
@@ -9,11 +9,11 @@ urlpatterns = [
     path('profile', views.accssemyprofile_action, name='profile_page'),
     path('othersprofile/<int:id>', views.accessothers_action, name='others_profile_page'),
     path('confirm-registration/<slug:username>/<slug:token>',
-        views.confirm_action, name='confirm'),
-    path('allusers',views.list_all_user,name='users_list'),
+         views.confirm_action, name='confirm'),
+    path('allusers', views.list_all_user, name='users_list'),
     path('photo/<int:id>', views.get_photo, name='photo'),
     path('create_repo', views.create_repo_action, name='create_repo'),
-    path('<str:repo_name>', views.repo_contents, name='repo_contents'),
+    path('repo_content/<str:repo_name>', views.repo_contents, name='repo_contents'),
     path('issue/<str:repo_name>', views.issue_list_page, name='issue_list_page'),
     path('issue_detail/<int:issue_id>', views.issue_detail_page, name='issue_detail_page'),
     path('create_issue_page/<str:repo_name>', views.create_issue_page, name='create_issue_page'),
@@ -22,5 +22,5 @@ urlpatterns = [
     path('tree/<str:repo_name>/<path:repo_path>', views.repo_contents, name='repo_contents'),
     path('blob/<str:repo_name>/<path:repo_path>', views.file_view, name='file_view'),
     path('commit/<str:repo_name>/<str:commit_id>', views.view_diff, name='view_diff'),
-    
+    path('all_unsolved_issues', views.list_all_unsolved_issue, name='all_unsolved_issues'),
 ]
